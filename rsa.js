d2f069f73e95f406092c2089e19c2b78a0c7c317
Kayo Phoenix <kayo@illumium.org>
2012-06-23 15:58:28 +0600

    Binary Encoding/Decoding functionality implemented by using Buffer objects.

--- rsa.js
+++ rsa.js
@@ -1,34 +1,71 @@
 // load core crypto to get OpenSSL initialized
-var crypto = require('crypto');
+var crypto = require('crypto'), rsa;
 
 try {
-    var binding = require('./rsaBinding');
-    var RsaKeypair = binding.RsaKeypair;
-    var rsa = true;
+  var binding = require('./rsaBinding'),
+  RsaKeypair = binding.RsaKeypair;
+  rsa = true;
 } catch (e) {
-    var rsa = false;
+  rsa = false;
 }
+
+var variants = function(o){
+  var r = [];
+  for(var n in o){
+    r.push(n);
+    r.push(', ');
+  }
+  r[r.length-2] = ' or ';
+  return r.join('');
+},
+binEnc = {
+  binary: 0,
+  base64: 0,
+  hex: 0
+},
+checkBinEnc = function(enc, func){
+  if(!(enc in binEnc))
+    throw new Error('RsaKeypair.'+func+' encoding can be '+
+                    variants(binEnc)+' but '+enc+' given!');
+},
+RsaKeypairP = RsaKeypair.prototype,
+origEncrypt = RsaKeypairP.encrypt,
+origDecrypt = RsaKeypairP.decrypt,
+origGetBignum = RsaKeypairP.getBignum;
+
+RsaKeypairP.encrypt = function(data, dec, enc){
+  checkBinEnc(enc, 'encrypt');
+  return new Buffer(origEncrypt.call(this, data, dec), 'binary').toString(enc);
+};
+RsaKeypairP.decrypt = function(data, dec, enc){
+  checkBinEnc(dec, 'decrypt');
+  return origDecrypt.call(this, new Buffer(data, dec).toString('binary'), 'binary', enc);
+};
+RsaKeypairP.getBignum = function(enc){
+  checkBinEnc(enc, 'getBignum');
+  return new Buffer(origGetBignum.call(this), 'binary').toString(enc);
+};
 
 exports.RsaKeypair = RsaKeypair;
 exports.createRsaKeypair = function(keys) {
-    var k = new RsaKeypair();
+  var k = new RsaKeypair();
 
-    if (keys.publicKey) {
-	k.setPublicKey(keys.publicKey);
-    }
-    
-    if (keys.privateKey) {
-	if (keys.passphrase) {
+  if (keys.publicKey) {
+	  k.setPublicKey(keys.publicKey);
+  }
+
+  if (keys.privateKey) {
+	  if (keys.passphrase) {
 	    k.setPrivateKey(keys.privateKey, keys.passphrase);
-	}
-	else {
+	  }
+	  else {
 	    k.setPrivateKey(keys.privateKey);
-	}
-    }
-    
-    if (keys.padding) {
-    k.setPadding(keys.padding);
-    }
+	  }
+  }
 
-    return k;
-}
+  if (keys.padding) {
+    k.setPadding(keys.padding);
+  }
+
+  return k;
+};
