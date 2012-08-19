5b9cd831a49a3fdadaececd7dfe8aca5b544f090
Kayo Phoenix <kayo@illumium.org>
2012-08-20 02:00:24 +0600

    Complete support..

--- rsa.js
+++ rsa.js
@@ -15,7 +15,7 @@ var variants = function(o){
     r.push(n);
     r.push(', ');
   }
-  r[r.length-2] = ' or ';
+  r.splice(r.length-2, 0, ' or ');
   return r.join('');
 },
 binEnc = {
@@ -31,13 +31,27 @@ checkBinEnc = function(enc, func){
 RsaKeypairP = RsaKeypair.prototype,
 origEncrypt = RsaKeypairP.encrypt,
 origDecrypt = RsaKeypairP.decrypt,
-origGetBignum = RsaKeypairP.getBignum;
+origGetBignum = RsaKeypairP.getBignum,
+origGetModulus = RsaKeypairP.getModulus,
+origGetExponent = RsaKeypairP.getExponent;
 
 RsaKeypairP.encrypt = function(data, dec, enc){
+  if(typeof dec != 'string'){
+    dec = 'utf8';
+  }
+  if(typeof enc != 'string'){
+    enc = 'binary';
+  }
   checkBinEnc(enc, 'encrypt');
   return new Buffer(origEncrypt.call(this, data, dec), 'binary').toString(enc);
 };
 RsaKeypairP.decrypt = function(data, dec, enc){
+  if(typeof dec != 'string'){
+    dec = 'binary';
+  }
+  if(typeof enc != 'string'){
+    enc = 'utf8';
+  }
   checkBinEnc(dec, 'decrypt');
   return origDecrypt.call(this, new Buffer(data, dec).toString('binary'), 'binary', enc);
 };
@@ -45,22 +59,36 @@ RsaKeypairP.getBignum = function(enc){
   checkBinEnc(enc, 'getBignum');
   return new Buffer(origGetBignum.call(this), 'binary').toString(enc);
 };
+RsaKeypairP.getModulus = function(enc){
+  if(typeof enc != 'string'){
+    enc = 'binary';
+  }
+  checkBinEnc(enc, 'getModulus');
+  return new Buffer(origGetModulus.call(this), 'binary').toString(enc);
+};
+RsaKeypairP.getExponent = function(enc){
+  if(typeof enc != 'string'){
+    enc = 'binary';
+  }
+  checkBinEnc(enc, 'getExponent');
+  return new Buffer(origGetExponent.call(this), 'binary').toString(enc);
+};
 
 exports.RsaKeypair = RsaKeypair;
 exports.createRsaKeypair = function(keys) {
   var k = new RsaKeypair();
 
   if (keys.publicKey) {
-	  k.setPublicKey(keys.publicKey);
+    k.setPublicKey(keys.publicKey);
   }
 
   if (keys.privateKey) {
-	  if (keys.passphrase) {
-	    k.setPrivateKey(keys.privateKey, keys.passphrase);
-	  }
-	  else {
-	    k.setPrivateKey(keys.privateKey);
-	  }
+    if (keys.passphrase) {
+      k.setPrivateKey(keys.privateKey, keys.passphrase);
+    }
+    else {
+      k.setPrivateKey(keys.privateKey);
+    }
   }
 
   if (keys.padding) {
